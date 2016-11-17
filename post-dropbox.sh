7f7c50902083c3f07b86e238dd935e9a21c20147
nicolasf <nicolas@mgig.fr>
2016-11-17 16:18:17 +0100

    Pas forcément Dropbox
    Moi-même, je n’utilise plus Dropbox…
    
    À adapter en fonction des besoins en changeant la ligne 10 et la 16

--- post-dropbox.sh
+++ post-dropbox.sh
@@ -2,13 +2,16 @@
 
 ## README
 # /!\ Ce script d'installation est conçu pour mon usage. Ne le lancez pas sans vérifier chaque commande ! /!\
-# Ce script est à utiliser après la synchronisation des données Dropbox
+# Ce script est à utiliser après la synchronisation des données Dropbox (ou Google Drive, ou iCloud, ou ce que vous voulez)
 
 echo "Installation de mackup et restauration des préférences."
 brew install mackup
+# Sélection du service de cloud (commenter si vous utilisez Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
+echo -e "[storage]\nengine = google_drive" >> ~/.mackup.cfg
+
 # Restore sans demander à chaque fois l'autorisation
 mackup restore -n
 
 echo "Configuration de FastScripts"
-defaults write com.red-sweater.FastScripts ScriptTreePathsKey '("~/Dropbox/Logiciels/Scripts")'
+defaults write com.red-sweater.FastScripts ScriptTreePathsKey '("~/Google Drive/Logiciels/Scripts")'
 
