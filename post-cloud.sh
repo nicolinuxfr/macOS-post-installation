8538fd07fb02d44bf5f1ed57141a2678f403a5b8
nicolasf <nicolas@mgig.fr>
2016-11-21 14:58:08 +0100

    Dropbox : Out / Google Drive : in
    Changement de nom pour le deuxième fichier, puisqu’il ne correspond
    plus nécessairement à Dropbox.

--- post-cloud.sh
+++ post-cloud.sh
@@ -6,11 +6,12 @@
 
 echo "Installation de mackup et restauration des préférences."
 brew install mackup
-# Sélection du service de cloud (commenter si vous utilisez Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
+# Sélection du service de cloud (à commenter si vous utilisez Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
 echo -e "[storage]\nengine = google_drive" >> ~/.mackup.cfg
 
-# Restore sans demander à chaque fois l'autorisation
+# Récupéeation de la sauvegarde sans demander à chaque fois l'autorisation
 mackup restore -n
+
 
 echo "Configuration de FastScripts"
 defaults write com.red-sweater.FastScripts ScriptTreePathsKey '("~/Google Drive/Logiciels/Scripts")'
